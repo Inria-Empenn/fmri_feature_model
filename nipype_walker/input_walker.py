002c3d75ab427a5ee7d3cb000ef63b7fe1ee1612
Youenn Merel <youenn.merel@inria.fr>
2025-01-17 17:14:32 +0100

    NiPype walker

--- nipype_walker/input_walker.py
+++ nipype_walker/input_walker.py
@@ -1,10 +1,14 @@
 import inspect
+import itertools
 
 import nipype.interfaces.spm.preprocess as spm_preprocess
 from nipype.interfaces.base.core import BaseInterface
-from nipype.interfaces.base.specs import BaseInterfaceInputSpec
+from nipype.interfaces.base.specs import TraitedSpec
 from nipype.interfaces.base.traits_extension import InputMultiPath, File
-from traits.trait_types import Range, Bool, Int, Float, String
+from traits.trait_types import Range, Bool, Int, Float, String, Enum, List, Str
+
+ignore_param = ['matlab_cmd', 'mfile', 'paths', 'trait_added', 'trait_modified', 'use_mcr', 'use_v8struct']
+
 
 def walk(cls):
     """
@@ -13,16 +17,19 @@ def walk(cls):
     :return:
     """
     json = {}
-    if not issubclass(cls, BaseInterfaceInputSpec):
+    if not issubclass(cls, TraitedSpec):
         return json
     for members in inspect.getmembers(cls):
         if members[0] == '__base_traits__':
             attributes = members[1]
             for key in attributes:
+                if key in ignore_param:
+                    continue
                 name = key
                 input_param = attributes[key].handler
                 json[name] = walk_param(input_param)
     return json
+
 
 def walk_param(trait):
     for member in inspect.getmembers(trait):
@@ -30,19 +37,29 @@ def walk_param(trait):
             attributes = member[1]
             json = parse_common_param_attributes(attributes)
             if isinstance(trait, Bool):
-                 json["type"] = "Boolean"
-            elif isinstance(trait, String):
-                 json["type"] = "String"
+                json["type"] = "Boolean"
+                json["default_value"] = parse_default_value(attributes)
+            elif isinstance(trait, String) or isinstance(trait, Str):
+                json["type"] = "String"
+                json["default_value"] = parse_default_value(attributes)
             elif isinstance(trait, Int):
                 json["type"] = "Integer"
+                json["default_value"] = parse_default_value(attributes)
             elif isinstance(trait, Float):
                 json["type"] = "Float"
+                json["default_value"] = parse_default_value(attributes)
             elif isinstance(trait, InputMultiPath):
                 json["type"] = "Files"
             elif isinstance(trait, File):
                 json["type"] = "File"
+            elif isinstance(trait, Enum):
+                json.update(parse_enum(attributes))
+                json["default_value"] = parse_default_value(attributes)
             elif isinstance(trait, Range):
-                 json.update(parse_range(attributes))
+                json.update(parse_range(attributes))
+                json["default_value"] = parse_default_value(attributes)
+            # elif isinstance(trait, List):
+            #     print("List")
             return json
     return None
 
@@ -55,13 +72,39 @@ def parse_common_param_attributes(attributes):
     :param attributes:
     :return:
     """
-    json = {"desc": "", "mandatory": False, "field": "", "default_value": None, "type" : "Unsupported"}
+    json = {"desc": "", "mandatory": False, "field": "", "default_value": None, "type": "Unsupported"}
     if "_metadata" in attributes:
         json["desc"] = attributes["_metadata"].get("desc", "")
         json["mandatory"] = attributes["_metadata"].get("mandatory", False)
         json["field"] = attributes["_metadata"].get("field", "")
-    json["default_value"] = attributes.get("default_value", None)
+        json["excludes"] = attributes["_metadata"].get("xor", [])
+        json["requires"] = attributes["_metadata"].get("requires", [])
     return json
+
+def parse_default_value(attributes):
+    if "_metadata" in attributes:
+        return attributes.get("default_value", None)
+
+
+def parse_enum(attributes):
+    oneof = [list([item for item in attributes.get("values", [])])]
+    json = {
+        "oneof": oneof
+    }
+    json["type"] = get_enum_type(json)
+    return json
+
+
+def get_enum_type(json):
+    test = json["oneof"][0]
+    if isinstance(test, float):
+        return "Float"
+    if isinstance(test, int):
+        return "Integer"
+    if isinstance(test, str):
+        return "String"
+    return "Unsupported"
+
 
 def parse_range(attributes):
     """
